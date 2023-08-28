bf7f87c638869d39d2ab60d37f90159eeb6c4146
DoctorLynch <doctor_linchenko@mail.ru>
2023-08-28 03:28:25 +0300

    HW1

--- homework-1/main.py
+++ homework-1/main.py
@@ -2,14 +2,14 @@
 import psycopg2
 
 #  connect to database
-conn = psycopg2.connect(host='localhost', database='test', user='postgres', password='02011999')
+conn = psycopg2.connect(host='localhost', database='north', user='postgres', password='02011999')
 try:
     with conn:
         with conn.cursor() as cur:
             # execute query
-            cur.execute('INSERT INTO user_account VALUES (%s, %s)', (7, 'Jake'))
-
-            cur.execute('SELECT * FROM user_account')
+            cur.execute('INSERT INTO employees VALUES (%s, %s, %s, %s, %s, %s)', (1, 'Nancy', 'Davolio' ,'Sales Representative', '1948-12-08', 'Education includes a BA in psychology from Colorado State University in 1970.  She also completed The Art of the Cold Call.  Nancy is a member of Toastmasters International.'
+))
+            cur.execute('SELECT * FROM employees')
             rows = cur.fetchall()
             for row in rows:
                 print(row)
