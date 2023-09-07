ac562f6891e32b6358d27558e9bdd6eec0b3c8c1
DoctorLynch <doctor_linchenko@mail.ru>
2023-09-08 00:21:10 +0300

    HW3

--- homework-3/queries.sql
+++ homework-3/queries.sql
@@ -1,11 +1,12 @@
 -- Напишите запросы, которые выводят следующую информацию:
 -- 1. Название компании заказчика (company_name из табл. customers) и ФИО сотрудника, работающего над заказом этой компании (см таблицу employees),
 -- когда и заказчик и сотрудник зарегистрированы в городе London, а доставку заказа ведет компания United Package (company_name в табл shippers)
-SELECT company_name as customer, CONCAT(employees.first_name, ' ', employees.last_name) AS employee
-FROM customers
-JOIN employees USING(city)
-FULL JOIN shippers USING(company_name)
-WHERE city='London' AND employees.reports_to=5
+SELECT customers.company_name AS customer, CONCAT(employees.first_name, ' ', employees.last_name) AS employee
+FROM orders
+INNER JOIN customers USING(customer_id)
+INNER JOIN employees USING(employee_id)
+INNER JOIN shippers ON shippers.shipper_id=orders.ship_via
+WHERE customers.city = 'London' AND employees.city = 'London' AND shippers.company_name = 'United Package';
 
 -- 2. Наименование продукта, количество товара (product_name и units_in_stock в табл products),
 -- имя поставщика и его телефон (contact_name и phone в табл suppliers) для таких продуктов,
@@ -29,5 +30,5 @@ ORDER BY company_name
 -- Этот запрос написать именно с использованием подзапроса.
 SELECT DISTINCT product_name
 FROM products
-JOIN order_details USING(product_id)
-WHERE quantity IN(SELECT 10 FROM order_details)
+INNER JOIN order_details USING(product_id)
+WHERE quantity IN(SELECT quantity FROM order_details WHERE quantity=10)
